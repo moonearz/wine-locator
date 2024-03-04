f86f8d95ba276a711c12f9068255ab46552978fe
moonearz <munirbenjemaa@gmail.com>
2024-03-04 08:56:10 -0500

    back to web server stuff

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+import fs from 'fs';
+
 document.addEventListener('DOMContentLoaded', ()=> {
     //search button click cues
     var skuInput = document.getElementById("sku");
@@ -31,16 +33,47 @@ function CSVtoHash(text, map) {
         var split = line.split(',');
         map.set(split[0], split.splice(1));
     }
+    /*
     for(var [key, value] of map) {
         console.log(key + " -> " + value[0]);
     }
+    */
 }
 
  //search button functions
  function skusearch(input, map) {
-    console.log(input);
+    if(map.has(input)) {
+        if(map.get(input)[1] === "wine") {
+            writeTest('test.csv', "this is a test");
+            window.open("wines.html")
+        }
+        else if(map.get(input)[1] === "beer") {
+            window.open("beers.html")
+        }
+        else {
+            alert("aisle not found!")
+        }
+    }
+    else {
+        alert("input not found!");
+    }
 }
 
 function namesearch(input, map) {
     console.log(input);
+}
+
+//marking/unmarking functions
+function writeTest(url, message) {
+    writeFile(url, message, (err) => {
+        if(err) throw err;
+    })
+}
+
+function mark(url, sku) {
+
+}
+
+function unmark(url) {
+
 }
